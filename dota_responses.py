41a4293f3ec2d5864d8f102b2b1fe0966cce76ad
= <=>
2020-02-09 11:01:36 +0530

    Added use of dota.dat file in getting url

--- dota_responses.py
+++ dota_responses.py
@@ -1,9 +1,12 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 from urllib.parse import unquote
 import pickle
 from urllib.parse import quote
 url = "https://dota2.gamepedia.com/"
+
+
 def get_hero_list():
     first_hero = '/Abaddon'
     last_hero = '/Zeus'
@@ -12,7 +15,8 @@ def get_hero_list():
         if res.status_code == 200:
             soup = BeautifulSoup(res.content, 'html.parser')
             links = soup.find_all('a')
-            hrefs = [link.get('href') for link in links if not (link.get('href') == '/Intelligence' or link.get('href') == '/Agility')]
+            hrefs = [link.get('href') for link in links if not (
+                link.get('href') == '/Intelligence' or link.get('href') == '/Agility')]
             heroes = hrefs[hrefs.index(first_hero):hrefs.index(last_hero)+1]
             heroes = [hero[1:] for hero in heroes]
             return heroes
@@ -22,12 +26,13 @@ def get_hero_list():
     except Exception as e:
         print(str(e))
 
+
 def prepare_response_from_text(text):
     text = [c for c in text if ord(c) < 255]
-    text = ''.join(text)          
+    text = ''.join(text)
     while text.startswith('Link'):
         text = text.strip(' Link')
-    tests = ['u' , 'r ']
+    tests = ['u', 'r ']
     for i in range(30, 301, 30):
         tests.append(str(i)+' ')
     for test in tests:
@@ -36,28 +41,32 @@ def prepare_response_from_text(text):
     text = text.strip()
     return text
 
+
 def get_hero_response(hero):
     try:
         res = requests.get(url+hero+'/Responses')
         if res.status_code == 200:
             soup = BeautifulSoup(res.content, 'html.parser')
             links = soup.find_all('li')
-            responses_link = [link for link in links if len(link.find_all('audio')) > 0]
+            responses_link = [link for link in links if len(
+                link.find_all('audio')) > 0]
             responses = {}
             for link in responses_link:
                 text = prepare_response_from_text(link.get_text())
-                
-                responses[text] = [a.get('href') for a in link.find_all('a') if a.get('href') != None and '.mp3' in a.get('href')]
-            
+
+                responses[text] = [a.get('href') for a in link.find_all(
+                    'a') if a.get('href') != None and '.mp3' in a.get('href')]
+
             return responses
         else:
             raise Exception(f"Error in fetching respose urls for Hero {hero}")
     except Exception as e:
         print(e)
 
+
 def download_resonse_mp3(response):
     try:
-        
+
         name = response.split('/')[-1]
         res = requests.get(response)
         if res.status_code == 200:
@@ -69,12 +78,27 @@ def download_resonse_mp3(response):
         print(str(e))
 
 
-def return_response_media_url(hero, response):
-    for k,v in get_hero_response(hero).items():
-        if k ==  response:
-            return v[0]
+def return_response_media_url(hero, response, filename='dota.dat'):
+    if os.path.exists(filename) and os.path.isfile(filename):
+        response_data = load_data_file(filename)
+        hero_ = beautify_hero_name(encode_hero_name(hero))
+        for hero in response_data:
+
+            if hero['name'] == hero_:
+
+                hero_response_data = hero['response_data']
+                for k, v in hero_response_data.items():
+                    if k == response:
+
+                        return v[0]
+
+    else:
+        hero = encode_hero_name(hero)
+        for k, v in get_hero_response(hero).items():
+            if k == response:
+                return v[0]
     return 'Not Found'
-        
+
 
 def beautify_hero_name(hero):
     hero = unquote(hero)
@@ -86,14 +110,16 @@ def beautify_hero_name(hero):
         hero = ' '.join(hero)
     return hero
 
+
 def make_all_data():
     dota = []
     for hero in get_hero_list():
         hero_name = beautify_hero_name(hero)
         response = get_hero_response(hero)
-        hero_data = {'name': hero_name, 'response_data':response}
+        hero_data = {'name': hero_name, 'response_data': response}
         dota.append(hero_data)
     return dota
+
 
 def encode_hero_name(hero):
     hero = hero.lower()
@@ -109,18 +135,15 @@ def encode_hero_name(hero):
         hero = quote(hero)
         return hero
 
+
 def make_data_file():
     with open('dota.dat', 'wb') as f:
-        pickle.dump(make_all_data(),f)
+        pickle.dump(make_all_data(), f)
 
     print("Data File Saved")
 
-def load_data_file(filname):
-    with open(filename) as f:
+
+def load_data_file(filename):
+    with open(filename, 'rb') as f:
         dota = pickle.load(f)
     return dota
-
-
-
-    
-    

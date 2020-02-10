fd133afc03a67c97ccd73a19b6ee8833ee86520e
Anubhav Mehra <anubhav.mehra@hotmail.com>
2020-02-10 16:40:22 +0530

    Added reddit.py

--- reddit.py
+++ reddit.py
@@ -1,7 +1,8 @@
 import requests
 import config
+import time
 
-from dota import dota_responses
+from dota_responses import *
 url_a = "https://www.reddit.com/"
 url_auth = "https://oauth.reddit.com/"
 
@@ -16,7 +17,7 @@ def get_access_token_object():
     except Exception as e:
         print(e)
 
-def get_private_messages(last_id= None):
+def get_private_messages(token_object, last_id= None):
     try:
         token_object = get_access_token_object()
         access_token = token_object['access_token']
@@ -26,14 +27,23 @@ def get_private_messages(last_id= None):
 
         if res.status_code != 200:
             raise Exception(f"Error getting messages, status returned {res.status_code}")
-        
-        for children in res.json()['data']['children']:
+        return res.json()['data']['children']
+    except Exception as e:
+        print(e)
+
+def reply_to_private_messages(token_object, message_list, last_id):
+    try:
+        token_object = get_access_token_object()
+        access_token = token_object['access_token']
+        token_type = token_object['token_type']
+        last_id = message_list[0]['kind']+"_"+message_list[0]['data']['id']
+        for children in message_list:
             data = children['data']
             fullname= children['kind']+"_"+data['id']
             body = data['body']
             author = data['author']
-            message = f" Hello {author}, This is bot. This is an automated reply. You wrote : {body}"
-            last_id = fullname
+            message = f"Hello {author}. This is a reply to your message containing the following: {body}. Thank you for messaging me. I am a bot under construction. My master says hello."
+            
             res = requests.post(url_auth+"api/comment", headers = {'User-Agent': 'bot-script by shifu_bot', 'Authorization': token_type.capitalize()+" "+access_token}, data = {'api_type': 'json', 'text': message, 'thing_id': fullname})
             if res.status_code != 200:
                 raise Exception(f"Error sending messages, response returned {res.json()}")   
@@ -43,5 +53,26 @@ def get_private_messages(last_id= None):
 
     except Exception as e:
         print(e)
+
+token_object = get_access_token_object()
+start = time.time()
+last_id = "t4_lwll0e"
+timeout  = 1
+while True:
+    try:
+        current = time.time()
+        if current - start >= token_object['expires_in']:
+            token_object = get_access_token_object()
+            start = time.time()
+            print("Token Renewed")
+        message_list = get_private_messages(token_object, last_id)
+        if len(message_list) > 0:
+            print("Message List Fetched...Replying...")
+            last_id = reply_to_private_messages(token_object, message_list, last_id)
+            print(f"Replied until id {last_id} ")
+        time.sleep(timeout*60)
+    except Exception as e:
+        print(e)
+        break
 
 
